48473b7cc163fc58c9aba95942e86f0fda59f951
elianismedina <elianismedina05@outlook.com>
2024-08-09 19:21:53 -0500

    finished desencriptar function

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var mensajeFinal = document.querySelector("#resultado");
 var btnCopiar = document.querySelector("#copiar");
 var muneco = document.querySelector("#imagen");
 var textoEncriptado = "";
+var textoDesencriptado = "";
 var textoaEncriptar = document.querySelector("#textousuario").value;
+var textoaDesencriptar = document.querySelector("#textousuario").value;
 
 
 
@@ -20,8 +22,24 @@ setInitialValues();
 
 btnEncriptar.addEventListener("click", function (event){
     event.preventDefault();
-    showResults();
+    showResultsEncriptar();
 });
+
+btnDesencriptar.addEventListener("click", function (event){
+    event.preventDefault();
+    showResultsDesencriptar();
+
+});
+
+btnCopiar.addEventListener("click", function (event){
+    event.preventDefault();
+    var copyText = document.querySelector("#resultado");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Mensaje copiado al portapapeles");
+
+});
+
 function replaceLetters(textoaEncriptar){
     switch(textoaEncriptar){
         case "e":
@@ -39,6 +57,22 @@ function replaceLetters(textoaEncriptar){
     }
 
 }
+function replaceLettersDesencriptar(textoaDesencriptar){
+    switch(textoaDesencriptar){
+        case "enter":
+            return "e";
+        case "imes":
+            return "i";
+        case "ai":
+            return "a";
+        case "ober":
+            return "o";
+        case "ufat":
+            return "u";
+        default:
+            return textoaDesencriptar;
+    }
+}
 function encriptar() {
     textoaEncriptar = document.querySelector("#textousuario").value;
 
@@ -54,12 +88,36 @@ function encriptar() {
         return textoEncriptado;
     }
 }
-function showResults(){
+function desencriptar(){
+    textoaDesencriptar = document.querySelector("#textousuario").value;
+
+    if(containsUppercase(textoaDesencriptar) || containsNumber(textoaDesencriptar) || containsSpecialCharacter(textoaDesencriptar)){
+        alert("El texto no debe contener mayusculas, numeros o caracteres especiales");
+
+    }
+    else{
+        console.log("Desencriptando...");
+        var textoValidoParaDesencriptar = textoaDesencriptar
+        var textoDesencriptado = textoValidoParaDesencriptar.replace(/enter|imes|ai|ober|ufat/g, replaceLettersDesencriptar);
+        console.log(textoDesencriptado);
+        return textoDesencriptado;
+    }
+}
+function showResultsEncriptar(){
     muneco.classList.add("hidden");
     header.innerHTML = "Tu mensaje fue encriptado";
     mensaje.innerHTML = "Tu mensaje encriptado es:";
     mensajeFinal.classList.remove("hidden");
     mensajeFinal.innerHTML = encriptar();
+    btnCopiar.classList.remove("hidden");
+
+}
+function showResultsDesencriptar(){
+    muneco.classList.add("hidden");
+    header.innerHTML = "Tu mensaje fue desencriptado";
+    mensaje.innerHTML = "Tu mensaje desencriptado es:";
+    mensajeFinal.classList.remove("hidden");
+    mensajeFinal.innerHTML = desencriptar();
     btnCopiar.classList.remove("hidden");
 
 }
